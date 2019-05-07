46d25ec025cd5f043a1fc2d83eed9c8f82c763b5
leogtzr <leogutierrezramirez@gmail.com>
2019-05-06 21:28:28 -0600

    add oauth2 / github

--- chat/auth.go
+++ chat/auth.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/objx"
 )
 
 type authHandler struct {
 	next http.Handler
 }
+
+var (
+	providers = map[string]struct{}{
+		"google":   {},
+		"facebook": {},
+		"github":   {},
+	}
+	actions = map[string]struct{}{
+		"login":    {},
+		"callback": {},
+		"github":   {},
+	}
+)
 
 func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	_, err := r.Cookie("auth")
@@ -30,4 +48,79 @@ func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 // MustAuth ...
 func MustAuth(handler http.Handler) http.Handler {
 	return &authHandler{next: handler}
+}
+
+/*
+/auth/login/google
+/auth/login/facebook
+/auth/callback/google
+/auth/callback/facebook
+*/
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	segs := strings.Split(r.URL.Path, "/")
+
+	if len(segs) < 4 {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "actions and providers not specified :(")
+		return
+	}
+
+	action := segs[2]
+	provider := segs[3]
+
+	if _, ok := providers[strings.ToLower(provider)]; !ok {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "Provider %s not supported", provider)
+		return
+	}
+
+	if _, ok := providers[strings.ToLower(provider)]; !ok {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "Action %s not supported", action)
+		return
+	}
+
+	switch action {
+	case "login":
+		provider, err := gomniauth.Provider(provider)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error when trying to get provider %s: %s", provider, err), http.StatusBadRequest)
+			return
+		}
+		loginURL, err := provider.GetBeginAuthURL(nil, nil)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error when trying to GetBeginAuthURL for %s:%s", provider, err), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("location", loginURL)
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	case "callback":
+		provider, err := gomniauth.Provider(provider)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error when trying to get provider %s: %s", provider, err), http.StatusBadRequest)
+			return
+		}
+		creds, err := provider.CompleteAuth(objx.MustFromURLQuery(r.URL.RawQuery))
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error when trying to complete auth for %s: %s", provider, err), http.StatusInternalServerError)
+			return
+		}
+		user, err := provider.GetUser(creds)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error when trying to get user from %s: %s", provider, err), http.StatusInternalServerError)
+			return
+		}
+		authCookieValue := objx.New(map[string]interface{}{
+			"name": user.Name(),
+		}).MustBase64()
+		http.SetCookie(w, &http.Cookie{
+			Name:  "auth",
+			Value: authCookieValue,
+			Path:  "/"})
+		w.Header().Set("Location", "/chat")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "Auth action %s not supported", action)
+	}
 }
--- chat/main.go
+++ chat/main.go
@@ -6,6 +6,9 @@ import (
 	"net/http"
 	"os"
 	"trace"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/github"
 )
 
 func main() {
@@ -13,11 +16,19 @@ func main() {
 	var addr = flag.String("addr", ":8080", "The addr of the application")
 	flag.Parse()
 
+	gomniauth.SetSecurityKey("a6da4e35e6b0cb614e93e33052dcf9596f21d23c")
+	gomniauth.WithProviders(
+		//facebook.New("key", "secret", "http://localhost:8080/auth/callback/facebook"),
+		github.New("81a518c207b48f360280", "a6da4e35e6b0cb614e93e33052dcf9596f21d23c", "http://localhost:8080/auth/callback/github"),
+		//google.New("key", "secret", "http://localhost:8080/auth/callback/google"),
+	)
+
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
 
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 
 	// get the room going
