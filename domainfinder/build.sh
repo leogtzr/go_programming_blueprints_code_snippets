5eb798b0f5e5ccac5a357340664c5d94009b43d4
leogtzr <leogutierrezramirez@gmail.com>
2019-05-26 14:19:27 -0600

    add programs to find synonyms

---
+++ domainfinder/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+set -x
+echo Building domainfinder... 
+go build -o domainfinder 
+
+echo Building synonyms... 
+cd ../synonyms 
+go build -o ../domainfinder/lib/synonyms 
+
+echo Building available... 
+cd ../available 
+go build -o ../domainfinder/lib/available 
+# cd ../build 
+
+echo Building sprinkle... 
+cd ../sprinkle 
+go build -o ../domainfinder/lib/sprinkle 
+
+# cd ../build 
+echo Building coolify... 
+
+cd ../coolify 
+go build -o ../domainfinder/lib/coolify 
+
+# cd ../build 
+echo Building domainify... 
+
+cd ../domainify 
+go build -o ../domainfinder/lib/domainify 
+
+# cd ../build 
+echo Done.
