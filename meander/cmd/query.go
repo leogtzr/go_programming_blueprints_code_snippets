aad1ddac5b009b13f1901d39a35227c5359320b6
lgutierr <leonardo.gutierrez@autozone.com>
2019-06-26 13:45:17 -0600

    add meander package

---
+++ meander/cmd/query.go
@@ -0,0 +1,23 @@
+package meander
+
+type Place struct {
+	*googleGeometry
+}
+
+type googleResponse struct {
+	Results []*Place `json:"results"`
+}
+
+type googleGeometry struct {
+	*googleLocation `json:"location"`
+}
+
+type googleLocation struct {
+	Lat float64 `json:"lat"`
+	Lng float64 `json:"lng"`
+}
+
+type googlePhoto struct {
+	PhotoRef string `json:"photo_reference"`
+	URL		string `json:"url"`
+}
