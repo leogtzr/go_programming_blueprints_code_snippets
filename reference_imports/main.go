5b1f3ac402893d33caf5da941dbf4894249f0c4a
leogtzr <leogutierrezramirez@gmail.com>
2019-07-15 09:51:10 -0600

    add reference import test project

---
+++ reference_imports/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Referencing something from the package so the compiler doesn't complain
+// about not using it ... 
+var _ = fmt.Println
+
+func main() {
+	
+}
